ff255be1b1746630d64bc7ce571eed1eb79bae42
tugane <tooamiel@gmail.com>
2023-02-02 21:53:29 +0200

    Movie

--- components/Movie.tsx
+++ components/Movie.tsx
@@ -1,4 +1,10 @@
-import { StyleSheet, TouchableOpacity, Image } from "react-native";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  StyleProp,
+  ViewStyle,
+} from "react-native";
 import React from "react";
 import Spacing from "../constants/Spacing";
 import { Movie as MovieType } from "../data";
@@ -6,22 +12,26 @@ import { Movie as MovieType } from "../data";
 interface Props {
   movie: MovieType;
   onPress?: () => void;
+  style?: StyleProp<ViewStyle>;
 }
 
 const movieHeight = 170;
 const movieWidth = 110;
 
-const Movie: React.FC<Props> = ({ movie, onPress }) => {
+const Movie: React.FC<Props> = ({ movie, onPress, style }) => {
   return (
     <TouchableOpacity
       onPress={onPress}
-      style={{
-        height: movieHeight,
-        width: movieWidth,
-        marginRight: Spacing,
-        overflow: "hidden",
-        borderRadius: Spacing / 2,
-      }}
+      style={[
+        {
+          height: movieHeight,
+          width: movieWidth,
+          marginRight: Spacing,
+          overflow: "hidden",
+          borderRadius: Spacing / 2,
+        },
+        style,
+      ]}
     >
       <Image source={movie.image} style={{ height: "100%", width: "100%" }} />
     </TouchableOpacity>
